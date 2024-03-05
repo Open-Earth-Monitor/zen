403156414e69b95602e09d53e417bcace63a3984
Rolf Simoes <rolfsimoes@gmail.com>
2024-03-05 09:46:17 +0100

    improve code and examples

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='zen',
-    version='0.1.0',
+    version='0.5.0',
     author='Rolf Simoes',
     author_email='rolf.simoes@opengeohub.org',
-    description='Zenodo API client and big dataset manangement',
+    description='Zenodo API client and big dataset management',
     packages=find_packages(),
     install_requires=open('requirements.txt').readlines()
 )
