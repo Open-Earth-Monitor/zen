22dba04598b28c4ee5bbdf96f81f517f40ea37dc
Rolf Simoes <rolfsimoes@gmail.com>
2024-10-01 20:55:39 +0200

    Add deploy.sh script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+[ ! -d "docs" ] && mkdir "docs"
+pip install -e .
+make clean
+make html
+rm -fr docs/*
+cp -r sphinx/_build/html/* docs/
+[ ! -f "docs/.nojekyll" ] && touch "docs/.nojekyll"
+git add docs
+git commit -m "Documentation deployment"
+git push origin dev
